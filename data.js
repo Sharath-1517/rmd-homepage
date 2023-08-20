e36a0f31913117b3a03976c094ef2bf62025c9c6
sharath-1517 <sharathprof1517@gmail.com>
2023-08-20 23:04:55 +0530

    navbar over

---
+++ data.js
@@ -0,0 +1,170 @@
+const data = [
+	
+		{
+		"name": "About Us",
+		"aboutLinks": [
+				{
+					"name": "RMK Group of institutions",
+					"link": "https://rmd.ac.in/aboutus/rmkgroup.html"
+				},
+				{
+					"name": "Vision & Mission",
+					"link": "https://rmd.ac.in/aboutus/vision.html"
+				},
+				{
+					"name": "Academic Programs",
+					"link": "https://rmd.ac.in/aboutus/academicprog.html"
+				},
+				{
+					"name": "Aprovals",
+					"link": "https://rmd.ac.in/aboutus/approvals.html"
+				},
+				{
+					"name": "Eligibility",
+					"link": "https://rmd.ac.in/aboutus/eligibility.html"
+				},
+				{
+					"name":"Genral conditions",
+					"link": "https://rmd.ac.in/aboutus/generalconditions.html"
+				},
+				{
+					"name": "Scholarships",
+					"link": "https://rmd.ac.in/aboutus/scholarships.html"
+				},
+				{
+					"name": "Distinguished Guests",
+					"link": "https://rmd.ac.in/aboutus/guest.html"
+				}
+			],
+	},
+	
+	{
+		name: "Adminstration",
+		"aboutLinks": [
+				{
+					"name": "Chairman",
+					"link": "https://rmd.ac.in/administration/chairman.html"
+				},
+				{
+					"name": "Vice Chairman",
+					"link": "https://rmd.ac.in/administration/vc.html"
+				},
+				{
+					"name": "Management Team",
+					"link": "https://rmd.ac.in/administration/managementteam.html"
+				},
+				{
+					"name": "Governing Board",
+					"link": "https://rmd.ac.in/administration/governig.html"
+				},
+				{
+					"name": "Principal",
+					"link": "https://rmd.ac.in/administration/principal.html"
+				},
+				{
+					"name": "Dean Research",
+					"link": "https://rmd.ac.in/administration/deanresearch.html"
+				},
+				{
+					"name": "Academic Dean",
+					"link": "https://rmd.ac.in/administration/academicdean.html"
+				}
+			],
+	},
+
+	{
+		name: "Academics",
+		"aboutLinks": [
+				{
+					"name": "Regulations 2021",
+					"link": "https://rmd.ac.in/academics/UG_regulations_2021.html"
+				},
+				{
+					"name": "Curriculam and Syllabus",
+					"link": "https://rmd.ac.in/academics/Curriculum.html"
+				},
+				{
+					"name": "Courses Offered",
+					"link": "https://rmd.ac.in/aboutus/rmkgroup.html"
+				},
+				{
+					"name": "Academic Schedule",
+					"link": "https://rmd.ac.in/academics/Academic_Schedule.html"
+				},
+				{
+					"name": "Code of Conduct",
+					"link": "https://rmd.ac.in/academics/Code_of_conduct.html"
+				},
+				{
+					"name": "Examination",
+					"link": "https://rmd.ac.in/academics/Examination.html"
+				}
+			],
+	},
+	{
+		name: "Departments",
+		"aboutLinks": [
+				{
+					"name": "CSE",
+					"link": "https://rmd.ac.in/dept/cse/index.html"
+				},
+				{
+					"name": "ECE",
+					"link": "https://rmd.ac.in/dept/ece/index.html"
+				},
+				{
+					"name": "EEE",
+					"link": "https://rmd.ac.in/dept/eee/index.html"
+				},
+				{
+					"name": "IT",
+					"link": "https://rmd.ac.in/dept/it/index.html"
+				},
+				{
+					"name": "CSBS",
+					"link": "https://rmd.ac.in/dept/csbs/index.html"
+				},
+				{
+					"name": "S&H",
+					"link": "https://rmd.ac.in/dept/snh/index.html"
+				},
+			],
+	},
+	{
+		"name": "Campus",
+		"aboutLinks": [
+				{
+					"name": "Genral Facilities",
+					"link": "https://rmd.ac.in/campus/facilities.html"
+				},
+				{
+					"name": "Green Campus",
+					"link": "https://rmd.ac.in/campus/greencampus.html"
+				},
+				{
+					"name": "Buildings",
+					"link": "https://rmd.ac.in/building.html"
+				},
+				{
+					"name": "Computer Centre",
+					"link": "https://rmd.ac.in/computercenter/index.html"
+				},
+				{
+					"name": "Central Library",
+					"link": "https://rmd.ac.in/library/index.html"
+				},
+				{
+					"name": "Sports",
+					"link": "https://rmd.ac.in/sports/index.html"
+				},
+				{
+					"name": "Hostel",
+					"link": "https://rmd.ac.in/campus/hostel.html"
+				},
+				{
+					"name": "Transport",
+					"link": "https://rmd.ac.in/campus/transport.html"
+				},
+			]
+		}
+]
